81de53f92df35dbff2ac2835632d5d80f0dff440
Leonardo <leonardosilvasantosss22@gmail.com>
2024-02-23 21:26:14 -0300

    Details interface

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,17 +1,45 @@
-import {Container} from './styles.js';
+import {Container, Links, Content} from './styles.js';
 import {Button} from '../../components/Button';
 import {Header} from '../../components/Header';
+import {Section} from '../../components/Section';
+import {Tag} from '../../components/Tag';
+import {ButtonText} from '../../components/ButtonText';
 
 export function Details() {
 
   return(
     <Container>
-      <Header>
-      </Header>
-        <h1>Hello World!</h1>
-        <span>Hallo!</span>
+      <Header />
+      
+      <main>
+        <Content>
+          <ButtonText title="Delete note" />
 
-      <Button title="Return" />
+          <h1>Introduction to React</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Illum accusantium culpa sunt hic dolorum. Harum minima quam nulla 
+            aliquam laborum, accusamus assumenda veniam alias, dolore,
+            tempore quis vitae consectetur dicta!
+          </p>
+
+          <Section title="Useful links">
+            <Links>
+              <li><a href="#">Link 1</a></li>  
+              <li><a href="#">Link 2</a></li>
+              <li><a href="#">Link 3</a></li>  
+            </Links>  
+          </Section>
+          
+          <Section title="Tags">
+            <Tag title="express" />
+            <Tag title="node.js" />
+          </Section>
+
+          <Button title="Return" />
+
+        </Content>
+      </main>
     </Container>
   );
 };
